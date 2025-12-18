057ec9662e2ea20460d9577a85dd00cd955e4c3e
Emanuel Magalhaes <emanuel.mag.jr@gmail.com>
2025-12-18 09:38:07 -0300

    chore(evil_twin): change path to save passwords

--- components/Applications/wifi/evil_twin.c
+++ components/Applications/wifi/evil_twin.c
@@ -31,7 +31,7 @@ static const char *TAG = "EVIL_TWIN_BACKEND";
 
 #define PATH_HTML_INDEX "/assets/html/captive_portal/evil_twin_index.html"
 #define PATH_HTML_THANKS "/assets/html/captive_portal/evil_twin_thank_you.html"
-#define PATH_PASSWORDS_JSON "/assets/storage/captive_portal/passwords.json"
+#define PATH_PASSWORDS_JSON "/storage/captive_portal/passwords.json"
 
 static SemaphoreHandle_t storage_mutex = NULL;
 
