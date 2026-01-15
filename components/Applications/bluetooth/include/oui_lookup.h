21da595b6f45dd0cba4317ccebb6aa0d5f10d27f
Emanuel Magalhaes <emanuel.mag.jr@gmail.com>
2026-01-15 11:31:03 -0300

    feat(oui_lookup): added oui lookup to know devices vendor

---
+++ components/Applications/bluetooth/include/oui_lookup.h
@@ -0,0 +1,23 @@
+// Copyright (c) 2025 HIGH CODE LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+
+#ifndef OUI_LOOKUP_H
+#define OUI_LOOKUP_H
+
+#include <stdint.h>
+
+const char* oui_get_vendor(const uint8_t *mac);
+
+#endif // OUI_LOOKUP_H
