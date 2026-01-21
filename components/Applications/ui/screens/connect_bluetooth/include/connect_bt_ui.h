12a976e3192767354ce5bf810f5545b160e0e49e
luis_thiago <lthiagofarias@gmail.com>
2026-01-20 22:19:57 -0300

    feat(ui): bluetooth connect screen

---
+++ components/Applications/ui/screens/connect_bluetooth/include/connect_bt_ui.h
@@ -0,0 +1,16 @@
+#ifndef CONNECT_BT_UI_H
+#define CONNECT_BT_UI_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "lvgl.h"
+
+void ui_connect_bt_open(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // CONNECT_BT_UI_H
