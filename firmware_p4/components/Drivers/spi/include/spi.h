5aee6f6c81649ba3b73a52063e6479623b148e23
Emanuel Magalhaes <emanuel.mag.jr@gmail.com>
2026-02-11 22:08:02 -0300

    refactor(spi_driver): modularize SPI bus Management for P4 and C5

--- firmware_p4/components/Drivers/spi/include/spi.h
+++ firmware_p4/components/Drivers/spi/include/spi.h
@@ -1,18 +1,3 @@
-// Copyright (c) 2025 HIGH CODE LLC
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
-
 #ifndef SPI_H
 #define SPI_H
 
@@ -23,6 +8,7 @@ typedef enum {
     SPI_DEVICE_ST7789,
     SPI_DEVICE_CC1101,
     SPI_DEVICE_SD_CARD,
+    SPI_DEVICE_BRIDGE,
     SPI_DEVICE_MAX
 } spi_device_id_t;
 
@@ -33,11 +19,10 @@ typedef struct {
     int queue_size;
 } spi_device_config_t;
 
-// Declarações das funções
-esp_err_t spi_init(void);
-esp_err_t spi_add_device(spi_device_id_t id, const spi_device_config_t *config);
+esp_err_t spi_bus_init(spi_host_device_t host, int mosi, int miso, int sclk);
+esp_err_t spi_add_device(spi_host_device_t host, spi_device_id_t id, const spi_device_config_t *config);
 spi_device_handle_t spi_get_handle(spi_device_id_t id);
 esp_err_t spi_transmit(spi_device_id_t id, const uint8_t *data, size_t len);
-esp_err_t spi_deinit(void);
+esp_err_t spi_bus_deinit(spi_host_device_t host);
 
 #endif
